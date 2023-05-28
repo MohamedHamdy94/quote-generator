50d10c8f4db0b3c961789b422a1406ab7786f688
Mohamed Hamdy Ahmed Mohamed <mohamedhamdyahmed94@gmail.com>
2023-05-28 21:39:14 +0200

    update the project

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # quote-generator
-
+https://github.com/MohamedHamdy94/quote-generator.git
 hello there, your task is to add social media share buttons, which can be used to share quote on different social media platforms such as whatsapp, twitter, and so on.
 
 fork the repository and start working on it.
--- src/App.js
+++ src/App.js
@@ -1,28 +1,42 @@
+import {
+  FacebookIcon,
+  FacebookShareButton,
+  LinkedinIcon,
+  LinkedinShareButton,
+  TwitterIcon,
+  TwitterShareButton,
+  WhatsappIcon,
+  WhatsappShareButton,
+} from 'react-share';
 import './App.css';
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 const App = () => {
-  const url = "https://api.quotable.io/random";
+  const url = 'https://api.quotable.io/random';
   let quoteData = {
-    content: "Let time be your only competitor.",
-    author: "Ahmed Saber"
-  }
-  const [quote, setQuote] = useState(quoteData)
+    content: 'Let time be your only competitor.',
+    author: 'Ahmed Saber',
+  };
+  const [quote, setQuote] = useState(quoteData);
 
   const generateQuote = () => {
     fetch(url)
-      .then(response => response.json())
-      .then(data => {
-        console.log(data)
-        setQuote(data)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setQuote(data);
       });
-  }
+  };
 
   const copy = () => {
-    navigator.clipboard.writeText(quote.author + " once said: " + quote.content)
-    alert('copied')
-  }
+    navigator.clipboard.writeText(
+      quote.author + ' once said: ' + quote.content
+    );
+    alert('copied');
+  };
 
+  const shareUrl = window.location.href;
+  const title = 'quote generator';
   return (
     <>
       <h1>Quote Generator React App</h1>
@@ -30,13 +44,28 @@ const App = () => {
         <p>{quote.content}</p>
         <span>{quote.author}</span>
         <div className="btns">
-          <button onClick={copy} className="btn">Copy</button>
+          <button onClick={copy} className="btn">
+            Copy
+          </button>
           <button onClick={generateQuote}>Generate Another Quote</button>
+        </div>
+        <div className="Demo__some-network">
+          <FacebookShareButton url={shareUrl} >
+            <FacebookIcon size={32} round />
+          </FacebookShareButton>{' '}
+          <TwitterShareButton url={shareUrl} title={title}>
+            <TwitterIcon size={32} round />
+          </TwitterShareButton>{' '}
+          <WhatsappShareButton url={shareUrl} title={title} separator=":: ">
+            <WhatsappIcon size={32} round />
+          </WhatsappShareButton>{' '}
+          <LinkedinShareButton url={shareUrl}>
+            <LinkedinIcon size={32} round />
+          </LinkedinShareButton>
         </div>
       </div>
     </>
-  )
-}
-
+  );
+};
 
 export default App;
